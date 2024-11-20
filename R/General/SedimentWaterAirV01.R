6249605a0fe1f373753a857e44c718c526f299de
martie <andres-martinez@uiowa.edu>
2024-11-20 13:17:34 -0600

    updated version

--- R/General/SedimentWaterAirV01.R
+++ R/General/SedimentWaterAirV01.R
@@ -39,26 +39,27 @@ SedWatAirV01 = function(t, state, parms){
   # Congener-specific constants
   Kaw <- 0.01344142 # PCB 4 dimensionless Henry's law constant @ 25 C
   dUaw <- 49662.48 # internal energy for the transfer of air-water for PCB 4 (J/mol)
+  Kaw.t <- Kaw*exp(-dUaw/R*(1/Tw.1-1/Tst.1))*Tw.1/Tst.1
   Kow <- 10^(4.65) # PCB 4 octanol-water equilibrium partition coefficient
   dUow <-  -21338.96 # internal energy for the transfer of octanol-water for PCB 4 (J/mol)
-  Koa <- 10^(6.521554861) # PCB 4 octanol-air equilibrium partition coefficient
+  Kow.t <- Kow*exp(-dUow / R * (1 / Tw.1 -  1/ Tst.1))
+  Koa <- 10^(6.789298102) # PCB 4 octanol-air equilibrium partition coefficient
   
   # PUF constants 
   Apuf <- 7.07 # cm2
-  Vpuf <- 0.000029 # m3 volume of PUF
+  Vpuf <- 29 # cm3 volume of PUF
   Kpuf <- 10^(0.6366 * log10(Koa) - 3.1774)# m3/g PCB 4-PUF equilibrium partition coefficient
   d <- 0.0213*100^3 # g/m3 density of PUF
   
   # SPME fiber constants
   Af <- 0.138 # cm2/cm SPME area
-  Vf <- 0.000000069 # L/cm SPME volume/cm
+  Vf <- 0.000000069 * 1000 # cm3/cm SPME volume/cm
   L <- 1 # cm SPME length normalization to 1 cm
-  Kf <- 10^(1.06 * log10(Kow) - 1.16) # PCB 4-SPME equilibrium partition coefficient
+  Kf <- 10^(1.06 * log10(Kow.t) - 1.16) # PCB 4-SPME equilibrium partition coefficient
   
   # Sediment partitioning
   M <- 0.1 # kg/L solid-water ratio
   foc <- 0.03 # organic carbon % in particles
-  Kow.t <- Kow*exp(-dUow / R * (1 / Tw.1 -  1/ Tst.1))
   logKoc <- 0.94 * log10(Kow.t) + 0.42 # koc calculation
   K <- foc * 10^(logKoc) # L/kg sediment-water equilibrium partition coefficient
   
@@ -73,15 +74,13 @@ SedWatAirV01 = function(t, state, parms){
   SC.pcb.w <- v.H2O/D.pcb.water # Schmidt number PCB 4
   
   # kaw calculations (air-water mass transfer coefficient)
-  # i) Ka.w.t, ka.w corrected by water and air temps during experiment
-  Kaw.t <- Kaw*exp(-dUaw/R*(1/Tw.1-1/Tst.1))*Tw.1/Tst.1
-  # ii) Kaw.a, air-side mass transfer coefficient
+  # i) Kaw.a, air-side mass transfer coefficient
   Kaw.a <- V.water.air*(D.pcb.air/D.water.air)^(0.67) # [m/s]
-  # iii) Kaw.w, water-side mass transfer coefficient for PCB 4. 600 is the Schmidt number of CO2 at 298 K
+  # ii) Kaw.w, water-side mass transfer coefficient for PCB 4. 600 is the Schmidt number of CO2 at 298 K
   Kaw.w <- V.co2.w*(SC.pcb.w/600)^(-0.5) # [m/s]
-  # iv) kaw, overall air-water mass transfer coefficient for PCB 4
+  # iii) kaw, overall air-water mass transfer coefficient for PCB 4
   kaw.o <- (1/(Kaw.a*Kaw.t) + (1/Kaw.w))^-1 # [m/s]
-  # v) kaw, overall air-water mass transfer coefficient for PCB 4, units change
+  # iv) kaw, overall air-water mass transfer coefficient for PCB 4, units change
   kaw.o <- kaw.o*100*60*60*24 # [cm/d]
   
   # Bioavailability factor B
@@ -101,11 +100,11 @@ SedWatAirV01 = function(t, state, parms){
   Cw <- state[2]
   Ca <- state[3]
   
-  dCsdt <- (- f * kdf * Cs - (1 - f) * kds * Cs + ka * Cw) / B
-  dCwdt <- (- ka * Cw + f * kdf * Cs + (1 - f) * kds * Cs -
+  dCsdt <- - f * kdf * Cs - (1 - f) * kds * Cs + ka * Cw * K * M
+  dCwdt <- f * kdf * Cs + (1 - f) * kds * Cs - ka * Cw + 
               kaw.o * Aaw / Vw * (Cw - Ca / Kaw.t) -
-              kb * Cw) / B
-  dCadt <- (kaw.o * Aaw / Va * (Cw - Ca / Kaw.t))/ B # Ca = [ng/L]
+              kb * Cw / B
+  dCadt <- kaw.o * Aaw / Va * (Cw - Ca / Kaw.t) # Ca = [ng/L]
     
   # The computed derivatives are returned as a list
   return(list(c(dCsdt, dCwdt, dCadt)))
@@ -117,9 +116,14 @@ SedWatAirV01 = function(t, state, parms){
   Ct <- 630.2023 # ng/g PCB 4 sediment concentration
   M <- 0.1 # kg/L solid-water ratio
   Cs0 <- Ct * M * 1000 # [ng/L]
+  K <- 2128 # [L/kg]
+  Cs0 <- Ct / K * 1000 # @ equilibrium to be dissolved phase [ng/L]
+  
+  
+  
 }
 cinit <- c(Cs = Cs0, Cw = 0, Ca = 0) # [ng/L]
-parms <- list(kdf = 4, kds = 0.01, f = 0.6, ka = 400, kb = 0) # Input
+parms <- list(kdf = 1, kds = 0.01, f = 0.8, ka = 1, kb = 0) # Input
 t <- seq(from = 0, to = 40, by = 1)
 # Run the ODE function without specifying parms
 out.1 <- ode(y = cinit, times = t, func = SedWatAirV01, parms = parms)
@@ -130,24 +134,16 @@ head(out.1)
   colnames(df.1) <- c("time", "Cs", "Cw", "Ca")
   Vw <- 100 #[cm3]
   Va <- 125 # cm3
-  df.1$Mt <- (df.1$Cs + df.1$Cw) * Vw / 1000 + df.1$Ca * Va / 1000 # [ng]
-  df.1$fp <- (df.1$Cs * Vw / 1000) / df.1$Mt # [ng]
-  df.1$fw <- (df.1$Cw * Vw / 1000) / df.1$Mt # [ng]
-  df.1$fa <- (df.1$Ca * Va / 1000) / df.1$Mt # [ng]
+  
+  
+  df.1$Ms <- df.1$Cs * Vw / 1000 # = Cs * ms / M / 1000 [ng]
+  df.1$Mw <- df.1$Cw * Vw / 1000
+  df.1$Ma <- df.1$Ca * Va / 1000
+  df.1$Mt <- df.1$Ms + df.1$Mw + df.1$Ma  # [ng]
+  df.1$fp <- df.1$Ms / df.1$Mt * 100 # [ng]
+  df.1$fw <- df.1$Mw / df.1$Mt * 100 # [ng]
+  df.1$fa <- df.1$Ma / df.1$Mt * 100 # [ng]
 }
-
-# Create the plot with all
-ggplot(data = df.1, aes(x = time)) +
-  geom_line(aes(y = fp, color = "Sediment"), linewidth = 1) +       # Line for fp
-  geom_line(aes(y = fw, color = "Water"), linewidth = 1) +          # Line for Fw
-  geom_line(aes(y = fa, color = "Air"), linewidth = 1) +  # Line for Fa
-  labs(title = "Fraction vs Time", 
-       x = "Time", 
-       y = "Fraction") +
-  scale_color_manual(values = c("Sediment" = "blue", "Water" = "red",
-                                "Air" = "purple"),
-                     name = "Phase") +
-  theme_minimal()
 
 # Create the plot with sediment
 ggplot(data = df.1, aes(x = time)) +
