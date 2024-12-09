867907e32e62b1b91b878115b63cb0bf2c7d7272
martie <andres-martinez@uiowa.edu>
2024-12-09 12:48:07 -0600

    updaterd version.

--- R/RTM/S/AVL/V05/PCB17-RTMSTreatmentV05.R
+++ R/RTM/S/AVL/V05/PCB17-RTMSTreatmentV05.R
@@ -160,7 +160,7 @@ rtm.PCB17 = function(t, state, parms){
   # iv) kaw, overall air-water mass transfer coefficient for PCB 17, units change
   kaw.o <- kaw.o*100*60*60*24 # [cm/d]
   
-  # Add PCB sorption to LB400 and SPME (~ bioavailability factor)
+  # Add PCB sorption to LB400 (~ bioavailability factor)
   # General partition coefficient obtained from protein, lipid and
   # phospholipids %s
   # From UFZ-LSER database (calculate the biopartitioning)
@@ -169,7 +169,6 @@ rtm.PCB17 = function(t, state, parms){
   Clb400 <- 0.8 * 8 * 10^8 # [cell/mL]
   Vlb400 <- 1 # [um3/cell]
   Mlb400 <- Clb400 * Vlb400 * 10^-12 # [Llb400/Lw]
-  #B <- (1 + Klb400 * Mlb400 + Vf * Kf * L / Vw * 1000)
   B <- (1 + Klb400 * Mlb400)
 
   # Bioremediation rate
@@ -199,7 +198,7 @@ rtm.PCB17 = function(t, state, parms){
   dCwdt <- - ka * Cw + f * kdf * Cs + (1 - f) * kds * Cs -
     kaw.o * Aaw / Vw * (Cw - Ca / Kaw.t) - 
     ko * Af * L / Vw * (Cw - Cf / Kf) -
-    kb * Cw - kblb400 * (t <= 2) * Cw # [ng/L]
+    kb * Cw - kblb400 * (t <= 5) * Cw # [ng/L]
   dCfdt <- ko * Af / Vf * (Cw - Cf / Kf) # Cw = [ng/L], Cf = [ng/L]
   dCadt <- kaw.o * Aaw / Va * (Cw - Ca / Kaw.t) -
     ro * Apuf / Va * (Ca - Cpuf / Kpuf) # Ca = [ng/L]
@@ -217,7 +216,7 @@ rtm.PCB17 = function(t, state, parms){
 }
 cinit <- c(Cs = Cs0, Cw = 0, Cf = 0, Ca = 0, Cpuf = 0)
 parms <- list(ro = 540.409, ko = 10, kdf = 1.45, kds = 0.001, f = 0.8,
-              ka = 190, kb = 1, kblb400 = 320) # Input. From SPME calibration study, a decrease in PCB17 is seen.
+              ka = 190, kb = 1, kblb400 = 85) # Input. From SPME calibration study, a decrease in PCB17 is seen.
 t.1 <- unique(pcb_combined_control$time)
 # Run the ODE function without specifying parms
 out.1 <- ode(y = cinit, times = t.1, func = rtm.PCB17, parms = parms)
