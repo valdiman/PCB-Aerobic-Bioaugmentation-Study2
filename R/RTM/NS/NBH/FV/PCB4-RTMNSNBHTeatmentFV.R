2acb767726084231a8144e1f765b1ee9f619db7f
Martinez Araneda <martie@uiowa.edu>
2025-01-28 11:40:13 -0600

    updated version

--- R/RTM/NS/NBH/FV/PCB4-RTMNSNBHTeatmentFV.R
+++ R/RTM/NS/NBH/FV/PCB4-RTMNSNBHTeatmentFV.R
@@ -324,7 +324,7 @@ head(out.1)
     select(time, mf, mpuf)
   
   # Export data
-  write.csv(model_results_daily_clean, file = "Output/Data/RTM/NS/NBH/PCB4AVLSTreatmentFV.csv")
+  write.csv(model_results_daily_clean, file = "Output/Data/RTM/NS/NBH/PCB4NBHTreatmentFV.csv")
   
   # Prepare model data for plotting
   model_data_long <- model_results_daily_clean %>%
